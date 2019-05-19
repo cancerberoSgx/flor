ff3572b77f110acbfd3c7d8dd8d516b24879fa3a
Sebastián Gurín <sgurin@awalabs.com>
2019-05-19 07:05:52 -0300

    al

--- spec/autoLayoutSpec.tsx
+++ spec/autoLayoutSpec.tsx
@@ -1,10 +1,9 @@
-import { Flor, FlorDocument, YogaDocument, ProgramElement, debug, isElement, BorderStyle } from '../src';
-import { defaultTestSetup, willContain, expectToContain } from './testUtil';
-import { int, color } from './data';
+import { sleep } from 'misc-utils-of-mine-generic';
+import { BorderStyle, Flor } from '../src';
+import { AutoLayout } from '../src/util/layout/autolayout/autoLayout';
+import { color } from './data';
 import { FlorDocumentTesting } from './florTestHelper';
-import { sleep, array } from 'misc-utils-of-mine-generic';
-import { TODO, notFalsy } from 'misc-utils-of-mine-typescript';
-import { AutoLayout, extractSubViewBounds } from '../src/util/layout/autolayout/autoLayout';
+import { expectToContain, willContain } from './testUtil';
 
 describe('AutoLayout', () => {
   let flor: FlorDocumentTesting
@@ -15,15 +14,14 @@ describe('AutoLayout', () => {
     flor.destroy()
   })
 
-  fit('auto layout api', async done => {
+  it('auto layout api', async done => {
 
-   
     const el = flor.create(
-      <box width={44} height={15} top={2} left={2} 
-      border={{type: BorderStyle.round}}
+      <box width={44} height={15} top={2} left={2}
+        border={{ type: BorderStyle.round }}
       >
-    <box name="child1"  top={2} left={10} width={10} height={4} bg={color()}>child1</box>
-        <box name="child2"   top={4} left={20} width={10} height={4} bg={color()}>child2</box>
+        <box name="child1" top={2} left={10} width={10} height={4} bg={color()}>child1</box>
+        <box name="child2" top={4} left={20} width={10} height={4} bg={color()}>child2</box>
       </box>
     )
     flor.render()
@@ -45,24 +43,12 @@ describe('AutoLayout', () => {
   │                                          │
   ╰──────────────────────────────────────────╯
 `)
-
-
-// C:child1.top(>=0).left(>=0).bottom(<=parent.height).right(<=parent.width).width(>=0).height(>=0)//.height(<=parent.height).width(<=parent.width)
-// C:child2.top(>=0).left(>=0).bottom(<=parent.height).right(<=parent.width).width(>=0).height(>=0)//.height(<=parent.height).width(<=parent.width)
-    // var constraints =  `
-    //   H:|-[child1(==child2/2)]-[child2]-|
-    //   V:|-[child1]-|
-    //   V:|-[child2]-|
-    //   `.split('\n').map(s=>s.trim()).filter(notFalsy)
-
-      const constraints = [
-        'H:|[child1(==child2*0.5)][child2]|',
-        'V:|[child1,child2]|'
-      ] 
-//       V:|-[child1,child2]-|
-// H:|-[child1(==child2*0.5)][child2]-|
-      const l = new AutoLayout({parent: el, constraints, options:{ extended: true , spacing: 0} })
-      l.apply({fitContainerBounds: true})
+    const constraints = [
+      'H:|[child1(==child2*0.5)][child2]|',
+      'V:|[child1,child2]|'
+    ]
+    const l = new AutoLayout({ parent: el, constraints, options: { extended: true, spacing: 0 } })
+    l.apply({ fitContainerBounds: true })
     flor.render()
     await sleep(100)
 
@@ -84,19 +70,7 @@ describe('AutoLayout', () => {
   ╰──────────────────────────────────────────╯
 `)
 
-    // var c = require('autolayout').VisualFormat.parse([
-    //   'H:|[view1(==view2)]-10-[view2]|',
-    //   'V:|[view1,view2]|'
-    // ], {extended: true});
-    // var view = new (require('autolayout').View)({constraints: c});
-    // view.setSize(el.contentWidth, el.contentHeight);
-    // debug(extractBounds(view.subViews.view1)); // {left: 0, top: 0, width: 195, height: 500}
-    // debug(extractBounds(view.subViews.view2));// {left: 205, top: 0, width: 195, height: 500}
-    // debug(view.subViews.view2+''); // {left: 205, top: 0, width: 195, height: 500}
-    // await sleep(20000)
-    // , { extended: true, spacing: 1 });
 
-    
     done()
   })
 })
--- src/util/layout/autolayout/autoLayout.ts
+++ src/util/layout/autolayout/autoLayout.ts
@@ -1,8 +1,7 @@
-import { ProgramElement, isElement, Rectangle } from '../../../programDom';
+import { ProgramElement, isElement } from '../../../programDom';
 import { Constraint, AutoLayout as AutoLayoutType, ParseOptions, View, SubView } from './autoLayoutTypes';
 import { debug } from '../../logger';
 import { notFalsy } from 'misc-utils-of-mine-typescript';
-import { Rect } from '../../geometry';
 const AL = require('autolayout') as AutoLayoutType
 
 interface Options<E extends ProgramElement = ProgramElement> {
@@ -12,93 +11,41 @@ interface Options<E extends ProgramElement = ProgramElement> {
   children?: { [name: string]: E }
 }
 
+/**
+ * Define and apply layout to element's children using Apple's [Auto Layout](https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/AutolayoutPG/index.html) and [Visual Format Language](https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/AutolayoutPG/VisualFormatLanguage.html) through the library [autolayout](https://github.com/IjzerenHein/autolayout.js)
+ */
 export class AutoLayout<E extends ProgramElement = ProgramElement> {
   protected constraints: Constraint[];
   protected view: View
-  // parentConstraints: Constraint[];
   parent: E;
 
   constructor(options: Options<E>) {
     this.parent = options.parent
     this.constraints = typeof options.constraints[0] === 'string' ? AL.VisualFormat.parse(options.constraints as string[], options.options) : options.constraints as Constraint[]
     this.view = new AL.View({ constraints: this.constraints });
-    // this.parentConstraints = [
-    //   {
-    //     view1: "parent",
-    //     attr1: "width",
-    //     relation: "equ",
-    //     attr2: "width",
-    //     constant: this.parent.contentWidth,
-    //     priority: 999
-    //   },
-    //   {
-    //     view1: "parent",
-    //     attr1: "height",
-    //     relation: "equ",
-    //     attr2: "height",
-    //     constant: this.parent.contentHeight,
-    //     priority: 999
-    //   }
-    // ]
-    // this.view.addConstraints(this.parentConstraints)
-    // this.prepareChildren();
-    // this.view.setSize(this.parent.contentWidth, this.parent.contentHeight);
   }
 
+  /**
+   * Calculates and set children bounds corresponding to current parent content bounds.
+   */
   apply(options: { fitContainerBounds?: boolean } = {}) {
-    // this.parentConstraints[0].constant = this.parent.contentWidth
-    // this.parentConstraints[1].constant = this.parent.contentHeight
-    // this.prepareChildren();
-    // debug(this.parent.contentWidth, this.parent.contentHeight)
     this.view.setSize(this.parent.contentWidth, this.parent.contentHeight);
-    this.getBounds().forEach(b=>{
+    this.getBounds().forEach(b => {
       b.el.width = b.bounds.width
       b.el.height = b.bounds.height
       b.el.top = b.bounds.top
       b.el.left = b.bounds.left
     })
-    // this.prepareChildren(true);
-      if (options.fitContainerBounds) {
-        this.parent.contentWidth = Math.round(this.view.fittingWidth)
-        this.parent.contentHeight = Math.round(this.view.fittingHeight)
-      }
+    if (options.fitContainerBounds) {
+      this.parent.contentWidth = Math.round(this.view.fittingWidth)
+      this.parent.contentHeight = Math.round(this.view.fittingHeight)
+    }
   }
 
-  // private prepareChildren(modify=false) {
-  //   this.parent.childNodes.filter(isElement).forEach((c, i) => {
-  //     if (c.props.name) {
-  //       const v = this.view.subViews[c.props.name];
-  //       if (!v) {
-  //         console.log('Warning, no constraint found for child named ' + c.props.name);
-  //         return;
-  //       }
-  //       // debug(this.view.subViews.child1)
-  //       if(modify){
-  //         c.width = Math.round(v.width);
-  //       c.height = Math.round(v.height);
-  //       c.top = Math.round(v.top);
-  //       c.left = Math.round(v.left);
-  //       // debug(v.left, v.top, v.width, v.height)
-  //       }
-  //       // else {
-  //       //   if (c.width) {
-  //       //     v.intrinsicWidth = c.width;
-  //       //   }
-  //       //   if (c.height) {
-  //       //     v.intrinsicHeight = c.height;
-  //       //   }
-  //       // }
-  //     }
-  //     else {
-  //       debug('Warning, child without name will be ignored in the layout.');
-  //     }
-  //   });
-  // }
   /**
-   * same as 
+   * Gets children new bounds corresponding to current parent content bounds. Don't modify children's just returns the values. 
    */
-  getBounds( ): {bounds: {left: number, top: number, width: number, height: number}, el: E}[] {
-    // this.prepareChildren();
+  getBounds(): { bounds: { left: number, top: number, width: number, height: number }, el: E }[] {
     this.view.setSize(this.parent.contentWidth, this.parent.contentHeight);
     return this.parent.childNodes.filter(isElement).map((c, i) => {
       if (c.props.name) {
@@ -107,14 +54,15 @@ export class AutoLayout<E extends ProgramElement = ProgramElement> {
           console.log('Warning, no constraint found for child named ' + c.props.name);
           return;
         }
-          return {
-            el: c as E, 
-           bounds: { width: Math.round(v.width), 
+        return {
+          el: c as E,
+          bounds: {
+            width: Math.round(v.width),
             height: Math.round(v.height),
             top: Math.round(v.top),
             left: Math.round(v.left)
           }
-          }
+        }
       }
       else {
         debug('Warning, child without name will be ignored in the layout.');
@@ -127,73 +75,7 @@ export class AutoLayout<E extends ProgramElement = ProgramElement> {
 export function extractSubViewBounds(view: SubView) {
   return {
     left: view.left, top: view.top,
-    right: view.right, bottom: view.bottom, 
+    right: view.right, bottom: view.bottom,
     width: view.width, height: view.height
   }
 }
-
-
-/*
-
-function createAutoLayout(el: ProgramElement, visualConstraints: string[], options?: ParseOptions) {
-  var constraints = AutoLayout.VisualFormat.parse(visualConstraints, options);
-  var view = new AutoLayout.View({ constraints: constraints });
-  // view.setSize(el.contentWidth, el.contentHeight);
-  el.childNodes.filter(isElement).forEach((c, i) => {
-    if (c.props.name) {
-      const v = view.subViews[c.props.name]
-      if (!v) {
-        debug('Warning, no constraint found for child named ' + c.props.name);
-        return
-      }
-      if (c.width) {
-        v.intrinsicWidth = c.width
-      }
-      if (c.height) {
-        v.intrinsicHeight = c.height
-      }
-      // v.left = c.left v.top = c.top
-    } else {
-      debug('Warning, child without name will be ignored in the layout.');
-    }
-  })
-  view.setSize(el.contentWidth, el.contentHeight);
-  return {
-    getBounds( ) {
-      view.setSize(el.contentWidth, el.contentHeight);
-      return el.childNodes.filter(isElement).map((c, i) => {
-        if (c.props.name) {
-          const v = view.subViews[c.props.name]
-          if (!v) {
-            console.log('Warning, no constraint found for child named ' + c.props.name);
-            return
-          }
-          return extractBounds(v)
-        }else {
-          debug('Warning, child without name will be ignored in the layout.');
-        }
-      })
-    },
-    apply(options: {fitContainerBounds?: boolean} = {}) {
-      view.setSize(el.contentWidth, el.contentHeight);
-      if(options.fitContainerBounds){
-        el.contentWidth = Math.round(view.fittingWidth)
-        el.contentHeight = Math.round(view.fittingHeight)
-      }
-      el.childNodes.filter(isElement).forEach((c, i) => {
-        if (c.props.name) {
-          const v = view.subViews[c.props.name]
-          if (!v) {
-            console.log('Warning, no constraint found for child named ' + c.props.name);
-            return
-          }
-          c.width = Math.round(v.width)
-          c.height = Math.round(v.height)
-          c.top = Math.round(v.top)
-          c.left = Math.round(v.left)
-        }
-      })
-    }
-  }
-}
-*/
